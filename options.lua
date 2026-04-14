58700ced39071d91e6b3750cd88a851579e215bc
Charlie Chan <09charliechan@gmail.com>
2026-04-14 12:05:09 +0700

    insert | code of color set

--- options.lua
+++ options.lua
@@ -3,5 +3,8 @@ vim.opt.number = true
 vim.out.relativenumber = true
 vim.opt.cursorline = true
 
-
+-- true color support for the terminal 
+vim.opt.termguicolors = true
+-- set color of no. line
+vim.opt.nvim_set_hl(0, 'LineNr' , { fg = '#B0B0B9'})
 
