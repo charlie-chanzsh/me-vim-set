c6c59625354eb9d1ec16a2a42cc93c7e746d9854
Charlie Chan <09charliechan@gmail.com>
2026-04-14 12:11:51 +0700

    insert color-cusorlineNr+normal

--- options.lua
+++ options.lua
@@ -1,10 +1,14 @@
 -- basic setup
 vim.opt.number = true
-vim.out.relativenumber = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 -- true color support for the terminal 
 vim.opt.termguicolors = true
 -- set color of no. line
 vim.opt.nvim_set_hl(0, 'LineNr' , { fg = '#B0B0B9'})
+
+
+vim.opt.nvim_set_hl(0, 'CursorLineNr', { fg = '#FFFF00' , bold = true })
+vim.opt.nvim_set_hl(0, 'CursorLine', { fg = '#FFFF00' , bold = true })
 
