edbba9ef7efae24d864be1db2144615073c8115f
Charlie Chan <09charliechan@gmail.com>
2026-04-19 01:18:33 +0700

    add cpp -o

--- init.lua
+++ init.lua
@@ -11,3 +11,10 @@ end)
 
 vim.keymap.set('i', '<C-,>' , '<ESC>A' , { dosc = 'Jump to end if the line'})
 
+vim.keymap.set("n" , "<F5>" , function()
+	if vim.bo.filetype == "cpp" then
+		vim.cmd(":!g++ %:p -o /tmp/vim-autobuild.ot")
+		vim.cmd(':TermExec cmd="/tmp/vim-autobuild.out" position=float')
+	end 
+end, { noremap = true, silent = true })  -- set something for C++
+
